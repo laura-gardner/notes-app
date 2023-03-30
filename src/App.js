7a29cf7a5508b386f66ad2cb6449d07b65e7c570
laura-gardner <elgardner@gmail.com>
2023-03-30 11:55:01 +0100

    Add functionality to save notes to localStorage and display first line of notes in sidebar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from "react"
 import Sidebar from "./Sidebar"
 import Editor from "./Editor"
-import { data } from "./data"
 import Split from "react-split"
 import {nanoid} from "nanoid"
 
-/**
- * Challenge: Spend 10-20+ minutes reading through the code
- * and trying to understand how it's currently working. Spend
- * as much time as you need to feel confident that you 
- * understand the existing code (although you don't need
- * to fully understand everything to move on)
- */
-
 export default function App() {
-    const [notes, setNotes] = React.useState([])
+
+    const [notes, setNotes] = React.useState(() => {
+        return JSON.parse(localStorage.getItem("notes")) || []
+    })
+        
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
     )
+
+    React.useEffect(() => {
+        const notesStringified = JSON.stringify(notes)
+        localStorage.setItem("notes", notesStringified)
+    }, [notes])
+
     
     function createNewNote() {
         const newNote = {
@@ -41,7 +42,7 @@ export default function App() {
             return note.id === currentNoteId
         }) || notes[0]
     }
-    
+
     return (
         <main>
         {
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Sidebar(props) {
     const noteElements = props.notes.map((note, index) => (
-        <div key={note.id}>
+            <div key={note.id}>
             <div
                 
                 className={`title ${
@@ -10,7 +10,7 @@ export default function Sidebar(props) {
                 }`}
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
-                <h4 className="text-snippet">Note {index + 1}</h4>
+                <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
             </div>
         </div>
     ))
